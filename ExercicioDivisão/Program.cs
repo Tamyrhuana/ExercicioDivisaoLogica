8a5ed86611ff9c64dfb178a22e5f16fe87ca8edf
tamyresrsm <tamyres.rsmagalhaes@senacsp.edu.br>
2025-03-20 21:21:32 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioDivisão/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExercicioDivisão
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digito o primeiro número");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double divisao =primeiroNumero / segundoNumero;
+
+            Console.WriteLine("A divisão " + divisao);
+
+        }
+    }
+}
